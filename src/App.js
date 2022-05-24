e37685a80e87789868b02ae19bfef2a3bf786571
DianaY-a11y <80307545+DianaY-a11y@users.noreply.github.com>
2022-05-24 19:53:53 -0400

    New pages

--- src/App.js
+++ src/App.js
@@ -1,76 +1,19 @@
-import profile from './assets/images/profile.jpg';
+import React from 'react';
 import './styles/App.css';
-import styled from 'styled-components'
+import Home from './Home';
+import Art from './Art';
+import NavBar from './NavBar';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
-const Container = styled.div`
-  display: flex;
-  align-items: center;
-  width: 50%;
-
-  @media (max-width: 768px) {
-    flex-direction: column;
-    width: 80%;
-  }
-`
-
-const ProfileImage = styled.img`
-  height: 200px;
-  width: 200;
-  border-radius: 75px;
-  margin: 0px 50px 0px 0px;
-
-  @media (max-width: 768px) {
-    margin-bottom: 20px;
-    margin-right: 0px;
-  }
-`
-
-const IntroContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-`
-
-const Title = styled.h1`
-  margin: 0px;
-  font-size: 2em;
-`
-
-const Paragraph = styled.p`
-  font-size: 0.9em;
-  text-align: left;
-  margin: 5px 0px;
-`
-
-const LinkContainer = styled.div`
-  display: flex;
-  width: 100%;
-`
-
-const Link = styled.a`
-  font-size: 0.8em;
-  font-weight: 500;
-  color: #50343E;
-  margin-right: 10px;
-`
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-main">
-        <Title>🌟 Hey! I'm <span className="special-text">Diana Yue</span></Title>
-        <Container >
-          <ProfileImage src={profile} alt="Profile Picture"/>
-          <IntroContainer>
-            <Paragraph>I'm a Beijinger from Cincinnati. Also a current student studying computer science and philsophy at Harvard College. I'm obsessed with all films 🎥 and painting 🎨</Paragraph>
-            <LinkContainer>
-              <Link href="mailto:dianayue@college.harvard.edu">Email Me</Link>
-              <Link href="https://github.com/DianaY-a11y?tab=repositories">Github Work</Link>
-            </LinkContainer>
-          </IntroContainer>
-        </Container>
-        </div>
-    </div>
+
+      <div className="App">
+        <NavBar />
+        <Home />
+      </div>
+
   ); 
 }
 
---
+++ src/NavBar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import './styles/NavBar.css';
+import styled from 'styled-components';
+import logo from './assets/images/logo.png';
+
+const Logo = styled.img`
+  height: 95px;
+  width: 130px;
+  margin: 10px 20px 20px 20px;
+`
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="logo">
+      <Logo src={logo} alt="Logo"/>
+      </div>
+      <nav className="items">
+        <ul className='ul'>
+          <h1>
+          <Link to="/">Home</Link>
+          </h1>
+          <h1>
+          <Link to="/Art">Art</Link>
+          </h1>
+        </ul>
+      </nav>
+      </div>
+
+  );
+}
+
+export default Navbar;
