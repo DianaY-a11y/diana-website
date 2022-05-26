c6839849cf1c76b35f8d2b19e4cf8ebcf4801d34
DianaY-a11y <80307545+DianaY-a11y@users.noreply.github.com>
2022-05-26 15:20:46 -0400

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Home from "./Home";
 import NavBar from "./NavBar";
 import "./styles/App.css";
 import Art from "./Art";
-import Projects from "./Projects";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Art" element={<Art />} />
-          <Route path="/Projects" element={<Projects />} />
         </Routes>
       </Router>
     </div>
